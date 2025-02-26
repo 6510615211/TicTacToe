2eaf931a29f9b15ff0c4f5c3a53d32ae42152f45
ivy <6510615211@student.tu.ac.th>
2025-02-26 13:41:52 +0700

    Initial Commit

---
+++ TicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by นางสาวพรนัชชา ประทีปสังคม on 26/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by นางสาวพรนัชชา ประทีปสังคม on 26/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
